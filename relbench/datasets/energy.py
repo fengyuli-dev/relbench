3e528aa0b1479c1dacaf6c2c98cdf6484432b0f5
fengyuli <fengyuli2002@gmail.com>
2025-03-02 14:07:35 -0800

    Create database

--- relbench/datasets/energy.py
+++ relbench/datasets/energy.py
@@ -1,11 +1,6 @@
-import os
-import shutil
-from pathlib import Path
-
 import pandas as pd
 
 from relbench.base import Database, Dataset, Table
-from relbench.utils import decompress_gz_file
 
 
 class EnergyDataset(Dataset):
@@ -18,6 +13,9 @@ class EnergyDataset(Dataset):
         entity_ownership = pd.read_excel(local_file, sheet_name="Entity Ownership")
         coal_plant_ownership = pd.read_excel(
             local_file, sheet_name="Coal Plant Ownership"
+        )
+        gas_pipeline_ownership = pd.read_excel(
+            local_file, sheet_name="Gas Pipeline Ownership"
         )
         bioenergy_power_ownership = pd.read_excel(
             local_file, sheet_name="Bioenergy Power Ownership"
@@ -34,7 +32,41 @@ class EnergyDataset(Dataset):
         )
 
         tables = {}
-        tables["all_entities"] = Table(df=all_entities, pkey_col="Entity ID")
+        tables["all_entities"] = Table(
+            df=all_entities, fkey_col_to_pkey_table={}, pkey_col="Entity ID"
+        )
+        tables["entity_ownership"] = Table(
+            df=entity_ownership, fkey_col_to_pkey_table={}, pkey_col="Subject Entity ID"
+        )
+        tables["coal_plant_ownership"] = Table(
+            df=coal_plant_ownership,
+            fkey_col_to_pkey_table={"Owner GEM Entity ID": "all_entities"},
+        )
+        tables["gas_pipeline_ownership"] = Table(
+            df=gas_pipeline_ownership,
+            fkey_col_to_pkey_table={"Owner GEM Entity ID": "all_entities"},
+        )
+        tables["bioenergy_power_ownership"] = Table(
+            df=bioenergy_power_ownership,
+            fkey_col_to_pkey_table={"Owner GEM Entity ID": "all_entities"},
+        )
+        tables["coal_mine_ownership"] = Table(
+            df=coal_mine_ownership,
+            fkey_col_to_pkey_table={"Owner GEM Entity ID": "all_entities"},
+        )
+        tables["iron_min_ownership"] = Table(
+            df=iron_min_ownership,
+            fkey_col_to_pkey_table={"Owner GEM Entity ID": "all_entities"},
+        )
+        tables["gas_pipeline_ownership"] = Table(
+            df=gas_pipeline_ownership,
+            fkey_col_to_pkey_table={"Owner GEM Entity ID": "all_entities"},
+        )
+        tables["steel_plant_ownership"] = Table(
+            df=steel_plant_ownership,
+            fkey_col_to_pkey_table={"Owner GEM Entity ID": "all_entities"},
+        )
+        return Database(tables)
 
 
 if __name__ == "__main__":
